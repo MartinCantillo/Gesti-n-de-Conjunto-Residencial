186e9f31a7927bf6073d3fbea5cb34efcf05d5c4
OmarHdzO <omare-hernandezo@unilibre.edu.co>
2023-11-28 01:09:01 -0500

    Validaciones in report add

--- lib/Views/screens/Report/report_screen.dart
+++ lib/Views/screens/Report/report_screen.dart
@@ -31,6 +31,7 @@ final appState = AppState();
 
 // ignore: camel_case_types
 class reporte extends StatefulWidget {
+  
   const reporte({super.key});
 
   static const String nombre = 'reporte';
@@ -41,11 +42,13 @@ class reporte extends StatefulWidget {
 
 // ignore: camel_case_types
 class _reporteState extends State<reporte> {
+  final _formkey = GlobalKey<FormState>();
   final typeController = TextEditingController();
   final subjectController = TextEditingController();
   final descriptionController = TextEditingController();
   final List<File> _evidences = [];
   final TypeAnomaly = [
+    "Seleccionar",
     "Infrastructura",
     "Seguridad",
     "Incidente medico",
@@ -86,9 +89,11 @@ class _reporteState extends State<reporte> {
         anomaly: selectedval,
       ),
     ];
-
-    appState.reports.addAll(reports);
-    Navigator.of(context).popAndPushNamed(MyHomePage.nombre);
+    if(_formkey.currentState!.validate()){
+      appState.reports.addAll(reports);
+      Navigator.of(context).popAndPushNamed(MyHomePage.nombre);
+    }
+    
   }
 
   @override
@@ -110,117 +115,136 @@ class _reporteState extends State<reporte> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 25),
-            const Text("Registro Reporte",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              shadows: [
-                Shadow(
-                  blurRadius: 0.5
-                ),
-              ],
-              fontSize: 25
-            ),
-            ),
-            const SizedBox(height: 25),
-            Divider(),
-            const SizedBox(height: 25.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: DropdownButtonFormField(
-                value: selectedval,
-                items:TypeAnomaly.map(
-                (e) => DropdownMenuItem(value: e,child: Text(e),)).toList(),
-                onChanged: (val){
-                  setState(() {
-                    selectedval= val as String;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: "Tipo",
-              enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey.shade400),
-              ),
-              fillColor: Colors.grey.shade200,
-              filled: true,
-              )
-                ),
-            ),
-            const SizedBox(height: 16.0),
-            MyTextField(
-                controller: subjectController,
-                hintText: 'Asunto',
-                obscureText: false,
-                maxLines: 1,
-                labelText: "Asunto"
-            ),
-            const SizedBox(height: 16.0),
-            MyTextField(
-                controller: descriptionController,
-                hintText: 'Descripcion',
-                obscureText: false,
-                maxLines: 5,
-                labelText: "Descripcion",
-            ),
-            const SizedBox(height: 16.0),
-            Container(
-              height: 100,
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8)
-              ),
-                child: ElevatedButton(
-                  onPressed: _pickImage,
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle( 
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 25),
+              const Text("Registro Reporte",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                shadows: [
+                  Shadow(
+                    blurRadius: 0.5
                   ),
-                  child: const Text('Subir Evidencias'),
+                ],
+                fontSize: 25
+              ),
+              ),
+              const SizedBox(height: 25),
+              Divider(),
+              const SizedBox(height: 25.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: DropdownButtonFormField(
+                  value: selectedval,
+                  items:TypeAnomaly.map(
+                  (e) => DropdownMenuItem(value: e,child: Text(e),)).toList(),
+                  onChanged: (val){
+                    setState(() {
+                      selectedval= val as String;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Tipo",
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
                 ),
-            ),
-            const SizedBox(height: 16.0),
-            SizedBox(
-              height: 100.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: _evidences.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.file(_evidences[index]),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Container(
-              height: 100,
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8)
-              ),
-                child: ElevatedButton(
-                  onPressed: _submitReport,
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle( 
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                ),
+                validator: (value){
+                    if (value == TypeAnomaly[0]) {
+                      return("Debe seleccionar el tipo de anomalia");
+                    }
+                    return null;
+                  },
                   ),
-                  child: const Text('Enviar Reporte'),
+              ),
+              const SizedBox(height: 16.0),
+              MyTextField(
+                  controller: subjectController,
+                  hintText: 'Asunto',
+                  obscureText: false,
+                  maxLines: 1,
+                  labelText: "Asunto",
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return("El asunto es requerido");
+                    }
+                  },
+              ),
+              const SizedBox(height: 16.0),
+              MyTextField(
+                  controller: descriptionController,
+                  hintText: 'Descripcion',
+                  obscureText: false,
+                  maxLines: 5,
+                  labelText: "Descripcion",
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return("Se necesita una descripcion del problema");
+                    }
+                  }
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                height: 100,
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8)
                 ),
-            ),
-          ],
+                  child: ElevatedButton(
+                    onPressed: _pickImage,
+                    style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle( 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    child: const Text('Subir Evidencias'),
+                  ),
+              ),
+              const SizedBox(height: 16.0),
+              SizedBox(
+                height: 100.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _evidences.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.file(_evidences[index]),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                height: 100,
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                  child: ElevatedButton(
+                    onPressed: _submitReport,
+                    style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle( 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    child: const Text('Enviar Reporte'),
+                  ),
+              ),
+            ],
+          ),
         ),
       ),
     );
