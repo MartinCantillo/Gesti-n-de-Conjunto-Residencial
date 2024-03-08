11eaf1aee6c7c588580e2e45c66d2be4ed36bb83
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-03-08 11:08:31 -0500

    AdministradorModelProvider ready

---
+++ lib/Models/Persona.dart
@@ -0,0 +1,56 @@
+class Persona {
+  String _nombre;
+  int _telefono;
+  String _email;
+  int _codigo;
+
+  // Constructor
+  Persona({
+    required String nombre,
+    required int telefono,
+    required String email,
+    required int codigo,
+
+    
+  })  : _nombre = nombre,
+        _telefono = telefono,
+        _email = email,
+        _codigo = codigo;
+
+  //Set and Get
+  set setNombre(String nombre) {
+    if (nombre == "") {
+      throw ("El nombre esta vacio");
+    }
+    _nombre = nombre;
+  }
+
+  String get getNombre => _nombre;
+
+  set setTelefono(int telefono) {
+    if (telefono <= 0) {
+      throw ("Por favor verifica");
+    }
+    _telefono = telefono;
+  }
+
+  int get getTelefono => _telefono;
+
+  set setEmail(String email) {
+    if (email == "") {
+      throw ("El email esta vacio");
+    }
+    _email = email;
+  }
+
+  String get getEmail => _email;
+
+  set setCodigo(int codigo) {
+    if (codigo <= 0) {
+      throw ("Por favor verifica");
+    }
+    _codigo = codigo;
+  }
+
+  int get getCodigo => _codigo;
+}
---
+++ lib/Provider/AdministradorModelProvider.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+
+import 'package:gestionresidencial/Models/Administrador.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod/riverpod.dart';
+
+class AdministradorModelProvider
+    extends StateNotifier<List<AdministradorModel>> {
+  final String endpoint = "https://backendmovil2-default-rtdb.firebaseio.com/";
+  AdministradorModelProvider(List<AdministradorModel> state) : super(state);
+
+  Future<bool> save(AdministradorModel data) async {
+    try {
+      final url = "$endpoint/Administrador.json";
+      final response = await http.post(Uri.parse(url), body: data.toJson());
+      if (response.statusCode == 200) {
+        state = [...state, data];
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      Exception(e);
+      return false;
+    }
+  }
+
+  Future<List<AdministradorModel>> getAll() async {
+    try {
+      final url = "$endpoint/Administrador.json";
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        String body = utf8.decode(response.bodyBytes);
+        final jsonData = jsonDecode(body);
+        final listData = Administrador.fromJsonList(jsonData);
+        state = listData.adminList;
+        return listData.adminList;
+      } else {
+        throw Exception("Ocurrió algo ${response.statusCode}");
+      }
+    } catch (e) {
+      throw Exception("Error $e");
+    }
+  }
+
+  Future<int> delete(String id) async {
+    try {
+      final url = '$endpoint/Administrador/$id.json';
+
+      final response = await http.delete(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        state.removeWhere((admin) => admin.idAdmin == id);
+        return 1;
+      } else {
+        throw Exception("Error ${response.statusCode}");
+      }
+    } catch (e) {
+      throw Exception("Error $e");
+    }
+  }
+
+  Future<bool> update(AdministradorModel data) async {
+    try {
+      final url = "$endpoint/Administrador.json";
+      final response = await http.put(Uri.parse(url), body: data.toJson());
+      if (response.statusCode == 200) {
+        //final decodeData = jsonDecode(response.body);
+        state[state.indexWhere((admin) => admin.idAdmin == data.idAdmin)] =
+            data;
+        return true;
+      } else {
+        throw ("Error ${response.statusCode}");
+      }
+    } catch (e) {
+      throw Exception("Error $e");
+    }
+  }
+  
+}
+//1 estado (StateNotifier), 2 tipo de estado(List<AdministradorModel>)
+final administradorProvider = StateNotifierProvider<AdministradorModelProvider, List<AdministradorModel>>((ref) {
+  return AdministradorModelProvider([]); 
+});
+
+
+
